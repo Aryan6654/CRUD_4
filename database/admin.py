1d7dcceaae489bba27661ca4dccc3338f54851f0
Aryan6654 <86788412+Aryan6654@users.noreply.github.com>
2021-07-15 15:04:07 +0530

    Delete database directory

--- database/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from database import models
-from .models import User
-
-# Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'address')
